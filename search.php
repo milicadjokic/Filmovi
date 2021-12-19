fd7bb55b07b4559fc13dd1f0547a6e39f3763ad3
milicadjokic <milica.djjokic@gmail.com>
2021-12-19 15:22:25 +0100

    Dodat search

---
+++ search.php
@@ -0,0 +1,21 @@
+<?php
+include('User.php');
+$search = ($_POST['search']);
+$users = new User();
+
+?>
+<head>
+    <title>Users</title>
+
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+<a href='index.php'>
+    <button class='btn btn-success my-3'>Home page</button>
+</a>
+</body>
+<?php while($rows = $users->searchUsers($search)): ?>
+
+<?php endwhile; ?>
